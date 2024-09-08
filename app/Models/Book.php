29ea5ed8a2e05a3570ac28720e4b1ed47cfbbb57
pointfocus <ghub@designbiz.aleeas.com>
2024-09-08 15:25:07 -0700

    added filament resources

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Observers\BookObserver;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 
+#[ObservedBy([BookObserver::class])]
 class Book extends Model
 {
     use HasFactory;
