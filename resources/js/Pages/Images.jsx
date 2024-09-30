bc9f03986d563e92d1a5ae0121747bb9ceb8aaeb
pointfocus <ghub@designbiz.aleeas.com>
2024-09-30 14:14:53 -0700

    added bendrey images2 folder and videos.txt for further details

--- resources/js/Pages/Images.jsx
+++ resources/js/Pages/Images.jsx
@@ -2,6 +2,8 @@ import Header from "../../components/header"
 import Footer from "../../components/Footer"
 import { useLanguage } from "../../providers/LanguageContext"
 import HeadComponent from "../../components/HeadComponent";
+import ImagesComponent from "../../components/ImagesComponent";
+
 
 export default function Images() {
     const { language } = useLanguage();
@@ -14,6 +16,7 @@ export default function Images() {
             <HeadComponent title={title} description={description} keywords={keywords} />
             <div className="flex flex-col min-h-screen justify-between bg-mainBg">
                 <Header title={language === "en" ? "Images" : "प्रतिमा"} />
+                <ImagesComponent />
                 <Footer />
             </div>
         </>
